d5221ef3cc026c28a28298a14296732c040dbaf2
Jorge Luis Guiza <jorge.guiza@silverapp.co>
2023-04-23 21:51:46 -0500

    🐛 Chore: Se arregla bug de importación en database. 🐛

--- src/app/database/connection.db.js
+++ src/app/database/connection.db.js
@@ -1,9 +1,9 @@
 
 import mongoose from "mongoose";
-import { database } from "../environments/database.env.js"
+import { DB_URL } from "../environments/database.env.js"
 
 try {
-    await mongoose.connect(database.DB_URL)
+    await mongoose.connect(DB_URL)
     console.log('👋 Conexión establecida con la base de datos. 👋');
 } catch (error) {
     console.log('❌ Conexión no establecida con la base de datos. ❌', error.message)
